58c6f4bc4e11ff3bb8afc2ecde1888a89751ea2e
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-05-31 15:48:22 +0530

    isolate environment and replace facebook with googlee

--- server.js
+++ server.js
@@ -5,21 +5,21 @@ var passport = require('passport');
 var Strategy = require('passport-google-oauth20').Strategy;
 
 
-// Configure the Facebook strategy for use by Passport.
+// Configure the Google strategy for use by Passport.
 //
 // OAuth 2.0-based strategies require a `verify` function which receives the
-// credential (`accessToken`) for accessing the Facebook API on the user's
+// credential (`accessToken`) for accessing the Google API on the user's
 // behalf, along with the user's profile.  The function must invoke `cb`
 // with a user object, which will be set at `req.user` in route handlers after
 // authentication.
 passport.use(new Strategy({
-    clientID: '368729150840-i15cfrtikv1ik7qhcqnh7o205rkkane0.apps.googleusercontent.com',
-    clientSecret: 'WwvCPe06Nb6uSNBwwXZJ_OAs',
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: '/return'
   },
   function(accessToken, refreshToken, profile, cb) {
-    // In this example, the user's Facebook profile is supplied as the user
-    // record.  In a production-quality application, the Facebook profile should
+    // In this example, the user's Google profile is supplied as the user
+    // record.  In a production-quality application, the Google profile should
     // be associated with a user record in the application's database, which
     // allows for account linking and authentication with other identity
     // providers.
@@ -34,7 +34,7 @@ passport.use(new Strategy({
 // production-quality application, this would typically be as simple as
 // supplying the user ID when serializing, and querying the user record by ID
 // from the database when deserializing.  However, due to the fact that this
-// example does not have a database, the complete Facebook profile is serialized
+// example does not have a database, the complete Google profile is serialized
 // and deserialized.
 passport.serializeUser(function(user, cb) {
   cb(null, user);
@@ -76,7 +76,7 @@ app.get('/login',
     res.render('login');
   });
 
-app.get('/login/facebook',
+app.get('/login/google',
   passport.authenticate('google', { scope: ['profile'] }));
 
 app.get('/return', 
